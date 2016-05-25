ad857e05cf2bbdd9379abe7ebb6dcdbd361c77b3
hydropal <capruitt09@gmail.com>
2016-05-24 21:59:23 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='hydro',
+      version='0.1',
+      description='Tools for analysis of the hydrologic system',
+      url='http://github.com/hydropal/hydro',
+      author='hydropal',
+      author_email='capruitt09@gamil.com',
+      license='',
+      packages=['hydro'],
+      zip_safe=False)
