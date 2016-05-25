e48aff6d26939e18ab714043db40c65071c5a156
hydropal <capruitt09@gmail.com>
2016-05-24 23:08:34 -0400

    added lyne_hollick filter

--- hydro/core.py
+++ hydro/core.py
@@ -1,5 +1,27 @@
 import numpy as np
 
+def Lyne_Hollick(series, alpha=.925, direction='f'):
+    """ Recursive digital filter for baseflow separation.
+    series = array of discharge measurements
+    alpha = filter parameter
+    direction = (f)orward or (r)everse calculation
+    """
+    series = np.array(series)
+    f = np.zeros(len(series))
+    if direction == 'f':
+        for t in np.arange(1,len(series)):
+            f[t] = alpha * f[t-1] + (1 + alpha)/2 * (series[t] - series[t-1])
+            if series[t] - f[t] > series[t]:
+                f[t] = 0
+    elif direction == 'r':
+        for t in np.arange(len(series)-2, 1, -1):
+            f[t] = alpha * f[t+1] + (1 + alpha)/2 * (series[t] - series[t+1])
+            if series[t] - f[t] > series[t]:
+                f[t] = 0
+
+    return np.array(series - f)
+
+
 def sinuosity(Easting, Northing, length, distance):
     """ Calculates sinuosity at each data point. Easting and Northing are lat/longs
     projected into measureable units. Length is distance to calculate the
