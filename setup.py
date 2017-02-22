192cdc591c9b5314a0429417da05b07940934045
hydrogeog <capruitt09@gmail.com>
2017-02-22 17:21:06 -0500

    update version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='hydro',
-      version='0.1',
+      version='0.2',
       description='Tools for analysis of the hydrologic system',
       url='http://github.com/hydropal/hydro',
       author='hydrogeog',
