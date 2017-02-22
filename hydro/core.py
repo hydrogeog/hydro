f2454296ee5d62ca4befff20f1ae4737752fb079
hydropal <capruitt09@gmail.com>
2017-02-22 16:17:25 -0500

    Created discharge and rating curve classes. General cleanup.

--- hydro/core.py
+++ hydro/core.py
@@ -1,105 +1,206 @@
-import numpy as np, pandas as pd
+import numpy as np, pandas as pd, matplotlib.pyplot as plt
+from datetime import datetime
+from matplotlib.ticker import FuncFormatter
+from scipy.optimize import curve_fit
+plt.style.use("seaborn-ticks")
 
 def exp_curve(x, a, b):
     """Exponential curve used for rating curves"""
     return (a * x**b)
 
-def ratingCurve(discharge, stage):
-    """Computes rating curve based on discharge measurements coupled with stage
-    readings.
-    discharge = array of measured discharges;
-    stage = array of corresponding stage readings;
-    Returns coefficients a, b for the rating curve in the form y = a * x**b
+def r_squ(x, y, pred):
+    """ 
+    Coefficient of determination
+    
+    x: independent variable\n
+    y: dependent variable\n
+    pred: predicted values
     """
-    from scipy.optimize import curve_fit
-    popt, pcov = curve_fit(exp_curve, stage, discharge)
+    a = 0.0
+    b = 0.0
+    ybar = np.mean(y)
+    for i, yhat in zip(y, pred):
+        a += (i - yhat)**2
+        b += (i - ybar)**2
+    return 1 - a / b
 
-    def r_squ():
-        a = 0.0
-        b = 0.0
-        for i, j in zip(discharge, stage):
-            a += (i - exp_curve(j, popt[0], popt[1]))**2
-            b += (i - np.mean(discharge))**2
-        return 1 - a / b
+class RC(object):
+    def __init__(self, stage, discharge):
+        """
+        Stage-Discharge rating curve
+        
+        stage: pandas series containing stage values correspoding to discharges\n
+        discharge: pandas series containing discharge measurements
+        """
+        self.stage = stage
+        self.discharge = discharge
+        # curve_fit
+        self.popt, self.pcov = curve_fit(exp_curve, self.stage, self.discharge)
+        # r-squared
+        self.pred = [exp_curve(j, self.popt[0], self.popt[1]) for j in self.stage]
+        self.r = r_squ(self.stage, self.discharge, self.pred)
 
-    return popt, r_squ()
+    def Q(self, allstages):
+        """ Compute discharges for entire series of stages"""
+        return list(map(lambda x: round(exp_curve(x, self.popt[0], self.popt[1]), 3), allstages))
 
-def dailyMean(Qseries, Tseries, minInterval):
-    """Takes the daily mean of a set of disharge data.
-    Qseries = series of disharge data;
-    Tseries = series of corresponding timestamps;
-    minInterval = time interval in minutes of the data;
-    Returns daily mean array and day array.
-    """
-    minInterval = int(1440/minInterval) # number of readings in a day
-    i=0; n=0; m=minInterval
-    dailyQ = np.zeros(int(len(Qseries)/minInterval)+1)
-    day = []
-    while i < len(Qseries)/minInterval:
-        try:
-            dailyQ[i] = np.nanmean(Qseries[n:m])        # calculate mean of each day
-            day.append(Tseries[n])                      # append date to list of dates
-            i+=1; n+=minInterval; m+=minInterval        # go to next day
-        except:
-            dailyQ[i] = np.nanmean(Qseries[n:])         # last day's mean
-            day.append(Tseries[n])
-    return dailyQ, np.array(day)
+    def plot(self, title='Rating Curve', log=True):
+        """ plot the rating curve """
+        fig = plt.figure()
+        ax1 = fig.add_subplot(111, facecolor='#E3E3E3')
+        plt.grid(True, which='both', color='w', ls='-', zorder=0)
+        ax1.scatter(self.stage, self.discharge, color='k', s=10)
+        ax1.set_ylabel(r'Discharge, cfs')
+        ax1.set_xlabel(r'Stage, ft')
+        if log:
+            ax1.set_ylim(0.01, 100)
+            ax1.set_yscale('log'); ax1.set_xscale('log')                              # log scale x and y
+            ax1.yaxis.set_major_formatter(FuncFormatter(lambda y,pos: ('{{:.{:1d}f}}'.format(int(np.maximum(-np.log10(y),0)))).format(y)))
+            ax1.xaxis.set_major_formatter(FuncFormatter(lambda y,pos: ('{{:.{:1d}f}}'.format(int(np.maximum(-np.log10(y),0)))).format(y)))
+        plt.title(title)
+        ax1.set_axisbelow(True)         # puts grid below plot
 
-def RB_Flashiness(series):
-    """Richards-Baker Flashiness Index for a series of daily mean discharges."""
-    Qsum = np.sum(series)           # sum of daily mean discharges
-    Qpath = 0.0
-    for i in range(len(series)):
-        if i == 0:
-            Qpath = series[i]       # first entry only
-        else:
-            Qpath += np.abs(series[i] - series[i-1])    # sum the absolute differences of the mean discharges
-    return Qpath/Qsum
+        # write the equation in the plot
+        ax1.text(0.05, 0.7, f'y = {self.popt[0]:.3f}x^{self.popt[1]:.3f}',
+                 fontsize=15, transform=ax1.transAxes)
+        # draw the model line
+        line = np.linspace(min(self.stage), max(self.stage), 100)
+        ax1.plot(line, exp_curve(line, self.popt[0], self.popt[1]), color='k')
+        plt.show()
+#        plt.savefig("/home/carson/Desktop/fig.png", bbox_inches='tight')
+
+class Discharge(object):
+    def __init__(self, time, Q, rain=None):
+        """
+        time: timeseries
+        Q: discharge values
+        """
+        self.time = time
+        self.Q = Q
+        self.rain = rain
+    
+    def dailyMean(self):
+        """
+        Takes the daily mean of a set of disharge data.
+        
+        Qseries = series of disharge data\n
+        Tseries = series of corresponding timestamps\n
+        Returns daily mean array and day array.
+        """
+        daily = pd.DataFrame({'Q':self.Q, 'time':self.time})
+        daily['day'] = daily.time.apply(lambda x: datetime(x.year, x.month, x.day))
+        daily = daily.groupby(['day'])['Q'].mean()
+        return daily
+
+    def RB_Flashiness(self):
+        """Richards-Baker Flashiness Index for a series of daily mean discharges."""
+        Qsum = np.sum(self.Q)           # sum of daily mean discharges
+        Qpath = 0.0
+        for i in range(len(self.Q)):
+            if i == 0:
+                Qpath = self.Q[i]       # first entry only
+            else:
+                Qpath += np.abs(self.Q[i] - self.Q[i-1])    # sum the absolute differences of the mean discharges
+        return Qpath/Qsum
 
 
-def flow_duration(series):
-    """Creates the flow duration curve for a discharge dataset. Returns a pandas
-    series whose index is the discharge values and series is exceedance probability."""
-    fd = pd.Series(series).value_counts()               # frequency of unique values
-    fd.sort_index(inplace=True)                         # sort in order of increasing discharges
-    fd = fd.cumsum()                                    # cumulative sum of frequencies
-    fd = fd.apply(lambda x: 100 - x/fd.max() * 100)     # normalize
-    return fd
+    def flow_duration(self, plot=False):
+        """
+        Creates the flow duration curve for a discharge dataset. Returns a pandas
+        series whose index is the discharge values and series is exceedance probability.
+        """
+        fd = pd.Series(self.Q).value_counts()               # frequency of unique values
+        fd.sort_index(inplace=True)                         # sort in order of increasing discharges
+        fd = fd.cumsum()                                    # cumulative sum of frequencies
+        fd = fd.apply(lambda x: 100 - x/fd.max() * 100)     # normalize
+        
+        if plot:
+            import probscale # flow duration curves use a probability scale for the x axis
+            fig = plt.figure()
+            ax1 = fig.add_subplot(1,1,1, axisbg='white')
+            ax1.plot(fd, fd.index, 'x', ls='', color='k', label='Total Flow', s=10)
+            
+            # set y axis to log scale and x axis to probability scale
+            ax1.set_yscale('log')
+            ax1.set_xscale('prob') # from import probscale
+            plt.xticks([.01,.1,.5,1,2,5,10,20,30,40,50,60,70,80,90,95,98,99,99.5,99.9,99.99],
+                       rotation='vertical')
+            plt.legend()
+            plt.title('Flow Duration Curve')
+            plt.ylabel('Flow (cfs)')
+            plt.xlabel('Percentage of time flow was equaled or exceeded')
+            plt.show()
+        return fd
 
-def Lyne_Hollick(series, alpha=.925, direction='f'):
-    """Recursive digital filter for baseflow separation. Based on Lyne and Hollick, 1979.
-    series = array of discharge measurements
-    alpha = filter parameter
-    direction = (f)orward or (r)everse calculation
-    """
-    series = np.array(series)
-    f = np.zeros(len(series))
-    if direction == 'f':
-        for t in np.arange(1,len(series)):
-            f[t] = alpha * f[t-1] + (1 + alpha)/2 * (series[t] - series[t-1])
-            if series[t] - f[t] > series[t]:
-                f[t] = 0
-    elif direction == 'r':
-        for t in np.arange(len(series)-2, 1, -1):
-            f[t] = alpha * f[t+1] + (1 + alpha)/2 * (series[t] - series[t+1])
-            if series[t] - f[t] > series[t]:
-                f[t] = 0
-    return np.array(series - f)
+    def Lyne_Hollick(self, alpha=.925, direction='f'):
+        """
+        Recursive digital filter for baseflow separation. Based on Lyne and Hollick, 1979.
+        
+        series : array of discharge measurements\n
+        alpha : filter parameter\n
+        direction : (f)orward or (r)everse calculation
+        """
+        self.Q = np.array(self.Q)
+        f = np.zeros(len(self.Q))
+        if direction == 'f':
+            for t in np.arange(1,len(self.Q)):
+                # algorithm
+                f[t] = alpha * f[t-1] + (1 + alpha)/2 * (self.Q[t] - self.Q[t-1])
+                # to prevent negative values
+                if self.Q[t] - f[t] > self.Q[t]:
+                    f[t] = 0
+        elif direction == 'r':
+            for t in np.arange(len(self.Q)-2, 1, -1):
+                f[t] = alpha * f[t+1] + (1 + alpha)/2 * (self.Q[t] - self.Q[t+1])
+                if self.Q[t] - f[t] > self.Q[t]:
+                    f[t] = 0
+        return np.array(self.Q - f)
 
-def Eckhardt(series, alpha=.98, BFI=.80):
-    """Recursive digital filter for baseflow separation. Based on Eckhardt, 2004.
-    series = array of discharge measurements
-    alpha = filter parameter
-    BFI = BFI_max (maximum baseflow index)
-    """
-    series = np.array(series)
-    f = np.zeros(len(series))
-    f[0] = series[0]
-    for t in np.arange(1,len(series)):
-        f[t] = ((1 - BFI) * alpha * f[t-1] + (1 - alpha) * BFI * series[t]) / (1 - alpha * BFI)
-        if f[t] > series[t]:
-            f[t] = series[t]
-    return f
+    def Eckhardt(self, alpha=.98, BFI=.80):
+        """
+        Recursive digital filter for baseflow separation. Based on Eckhardt, 2004.\n
+        series : array of discharge measurements\n
+        alpha : filter parameter\n
+        BFI : BFI_max (maximum baseflow index)
+        """
+        self.Q = np.array(self.Q)
+        f = np.zeros(len(self.Q))
+        f[0] = self.Q[0]
+        for t in np.arange(1,len(self.Q)):
+            # algorithm
+            f[t] = ((1 - BFI) * alpha * f[t-1] + (1 - alpha) * BFI * self.Q[t]) / (1 - alpha * BFI)
+            if f[t] > self.Q[t]:
+                f[t] = self.Q[t]
+        return f
+    
+    def plot(self, addseries=[], log=True):
+        """
+        Quick plot with or without rain data.\n
+        If you wish to plot more than one series to compare them, use addseries 
+        to list in order of [time, Q, ...] for each additional series.
+        """
+        fig = plt.figure()
+        ax1 = fig.add_subplot(111, facecolor='#E3E3E3')
+        plt.grid(True, which='both', color='w', ls='-', zorder=0)
+        ax1.plot(self.time, self.Q)
+        if type(self.rain) != None:
+            ax2 = ax1.twinx()
+            ax2.plot(self.time, self.rain, alpha=.5, c='b', lw=1)
+            ax2.set_ylim(1, 0)
+            ax2.set_ylabel(r'Rain, in')
+        ax1.set_ylabel('Discharge, cfs')
+        ax1.set_xlabel('Stage, ft')
+        # log scale for y axis
+        if log:
+            ax1.set_yscale('log')
+            ax1.yaxis.set_major_formatter(FuncFormatter(lambda y,pos: ('{{:.{:1d}f}}'.format(int(np.maximum(-np.log10(y),0)))).format(y)))
+        # add ablity to plot multiple time series
+        more = len(addseries)
+        while more > 0:
+            ax1.plot(addseries[more-2], addseries[more-1])
+            more -= 2
+        plt.show()
+
 
 def sinuosity(Easting, Northing, length, distance):
     """Calculates sinuosity at each data point. Easting and Northing are lat/longs
@@ -122,7 +223,7 @@ def sinuosity(Easting, Northing, length, distance):
         # Calculate sinuosity: stream length / straight line distance
         sin = np.zeros(len(East))
         l = len(East)
-        b = pnts * 2 * distance           # calculates stream distance for pnts in middle of dataset
+        b = pnts * 2 * distance  # calculates stream distance for pnts in middle of dataset
         for i in range(int(l)):
             if i < pnts: # first few points
                 a = (i + pnts) * distance # calculates stream distance
